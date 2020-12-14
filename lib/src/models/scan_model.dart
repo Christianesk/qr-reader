9d2aeb1eac4feecb41d8c8966179fe0e628b48b2
Christian Mena <christian.91.gm@hotmail.com>
2020-12-14 17:01:03 -0500

    Added model scan

---
+++ lib/src/models/scan_model.dart
@@ -0,0 +1,32 @@
+import 'package:meta/meta.dart';
+
+class ScanModel {
+    ScanModel({
+        this.id,
+        this.type,
+        @required this.value,
+    }){
+
+      if (this.type.contains('http')) {
+        this.type = 'type';
+      } else {
+        this.type = 'geo';
+      }
+    }
+
+    int id;
+    String type;
+    String value;
+
+    factory ScanModel.fromJson(Map<String, dynamic> json) => ScanModel(
+        id: json["id"],
+        type: json["type"],
+        value: json["value"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "type": type,
+        "value": value,
+    };
+}
